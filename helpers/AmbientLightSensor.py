bc78c2be115bb5a64157ec9d1a174f02eda79ecc
Joe Vacovsky Jr <vacovsky@gmail.com>
2016-06-11 14:19:20 -0700

    Fbisae

--- helpers/AmbientLightSensor.py
+++ helpers/AmbientLightSensor.py
@@ -20,14 +20,14 @@ class AmbientLightSensor:
         GPIO.setup(pin, GPIO.IN)
         # This takes about 1 millisecond per loop cycle
         while (GPIO.input(pin) == GPIO.LOW):
-            reading += 1
+            reading -= 1
         return reading
 
     def start(self, interval):
         while True:
             value = self.RCtime(self.pin_id, interval=1)
             RedisHelper().publish(self.channel, value)
-            print('Light Level: ', -1 * value, '                   ', end="\r", flush=True)
+            print('\nLight Level: ', value, '                   ', end="\r", flush=True)
 
 
 if __name__ == '__main__':
